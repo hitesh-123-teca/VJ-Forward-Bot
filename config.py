1671a9e431a84bfec34bf3635d7c1219565da53b
hiteshrathiya <hiteshrathiya123@gmail.com>
2025-11-02 13:04:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    API_ID = int(environ.get("API_ID", "21688431"))
+    API_HASH = environ.get("API_HASH", "db274cb8e9167e731d9c8305197badeb")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "8297730256:AAFOVC2WPyQO75IfSp6xjLLTRa3Jvt-S4JM") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://moviescorn:moviescorn@hitu.4jr5k.mongodb.net/?retryWrites=true&w=majority&appName=Hitu")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Hitu")
+    BOT_OWNER = int(environ.get("BOT_OWNER", "6861892595"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
@@ -27,3 +27,4 @@ class temp(object):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
