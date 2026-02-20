e0f49b60e0c74ec9f53c138f4e982ced83536ebf
hiteshrathiya <hiteshrathiya123@gmail.com>
2026-02-21 02:22:59 +0530

    Re-add comments and structure in config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     API_HASH = environ.get("API_HASH", "db274cb8e9167e731d9c8305197badeb")
     BOT_TOKEN = environ.get("BOT_TOKEN", "7852091851:AAE_FNIVvteZqSlrJerUVaNq13TWoOlDm0E") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://Forwardbot:<dbForwardbot@forward.3jlpu.mongodb.net/?retryWrites=true&w=majority&appName=Forward")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://Forwardbot:<dbForwardbot@forward.3jlpu.mongodb.net/?retryWrites=true&w=majority&appName=Forwardbot")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Forward")
     BOT_OWNER = int(environ.get("BOT_OWNER", "6861892595"))
 
@@ -27,6 +27,7 @@ class temp(object):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
