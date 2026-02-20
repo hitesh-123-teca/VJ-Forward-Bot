b383ae505172f7f58e1b6fd9ce33e4567c55a251
hiteshrathiya <hiteshrathiya123@gmail.com>
2026-02-21 02:00:25 +0530

    Update bot token and retain credits in config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from os import environ
 class Config:
     API_ID = int(environ.get("API_ID", "21688431"))
     API_HASH = environ.get("API_HASH", "db274cb8e9167e731d9c8305197badeb")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "8297730256:AAFOVC2WPyQO75IfSp6xjLLTRa3Jvt-S4JM") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7852091851:AAE_FNIVvteZqSlrJerUVaNq13TWoOlDm0E") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://moviescorn:moviescorn@hitu.4jr5k.mongodb.net/?retryWrites=true&w=majority&appName=Hitu")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Hitu")
@@ -27,4 +27,5 @@ class temp(object):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
