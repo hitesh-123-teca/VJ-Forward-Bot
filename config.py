63fdba1bc3c80e719065bd4ba9e88323ed62b739
hiteshrathiya <hiteshrathiya123@gmail.com>
2026-02-21 08:38:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from os import environ
 class Config:
     API_ID = int(environ.get("API_ID", "21688431"))
     API_HASH = environ.get("API_HASH", "db274cb8e9167e731d9c8305197badeb")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7852091851:AAE_FNIVvteZqSlrJerUVaNq13TWoOlDm0E") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7852091851:AAFBJjNRPlFf8LjtEkgybnuCQAWGqGwx51U") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://Forwardbot:<dbForwardbot@forward.3jlpu.mongodb.net/?retryWrites=true&w=majority&appName=Forwardbot")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Forward")
@@ -27,6 +27,7 @@ class temp(object):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
